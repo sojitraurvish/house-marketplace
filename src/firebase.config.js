883aff5e84cedf670b60c5af0852a20dd6afe5cb
urvish sojitra <sojitraurvish0@gmail.com>
2022-12-01 12:29:43 +0530

    initial commit

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-S7HKI7-6ZAToSIFLmjEstAmXXl79rRo",
+  authDomain: "house-market-app-45f03.firebaseapp.com",
+  projectId: "house-market-app-45f03",
+  storageBucket: "house-market-app-45f03.appspot.com",
+  messagingSenderId: "979526313341",
+  appId: "1:979526313341:web:169d00dea0203631599814"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
